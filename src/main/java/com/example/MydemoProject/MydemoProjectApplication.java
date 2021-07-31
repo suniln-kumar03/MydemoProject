dec1d5168bca0a87a28f916bec7ed750212ee6ca
Anubhav <anubhav@mztechgroup.com>
2021-07-31 19:26:28 +0530

    initial commit src

---
+++ src/main/java/com/example/MydemoProject/MydemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.MydemoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MydemoProject/MydemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MydemoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
