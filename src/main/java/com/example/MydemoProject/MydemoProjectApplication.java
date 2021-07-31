aaf95fc3e17286128edaf3c82cfd8480cbd784e2
Anubhav <anubhav@mztechgroup.com>
2021-07-31 21:02:07 +0530

    revert

--- src/main/java/com/example/MydemoProject/MydemoProjectApplication.java
+++ src/main/java/com/example/MydemoProject/MydemoProjectApplication.java
@@ -10,7 +10,7 @@ public class MydemoProjectApplication {
 
 //there is a lot of changes made to this code
 //there is a lot of changes made to this code
-
+//revert 2
 	public static void main(String[] args) {
 		SpringApplication.run(MydemoProjectApplication.class, args);
 	}
